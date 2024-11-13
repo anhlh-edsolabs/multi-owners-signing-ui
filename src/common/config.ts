87bd0f87dc65ae8da2455505adfa1e59fea02e09
Le Hoang Anh <anh.le@angelhack.com>
2024-11-13 18:08:06 +0700

    add config.ts

---
+++ src/common/config.ts
@@ -0,0 +1,20 @@
+import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
+// import { http, createConfig } from "wagmi";
+import { mainnet, sepolia } from "wagmi/chains"
+
+const projectId = import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID!
+
+// 2. Create wagmiConfig
+const metadata = {
+  name: 'Multi Owners signing',
+  description: 'Multi Owners signing',
+  url: 'https://web3modal.com', // origin must match your domain & subdomain
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
+}
+
+const chains = [mainnet, sepolia] as const
+export const config = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata,
+})
